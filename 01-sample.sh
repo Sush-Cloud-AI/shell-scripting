0b806874b1b75b34173425e8cfc4ba5d0a826241
Sush <sushcloud12@gmail.com>
2022-07-27 01:13:01 +0100

    Shell

--- 01-sample.sh
+++ 01-sample.sh
@@ -11,6 +11,13 @@ echo "HI"
 
 echo -e "Hello\n\tmonkey"
 
-#colour
+#colour fore ground
 
-echo -e "\e[33m I am yellow \e[0m"
+echo -e "\e[31mI am Red\e[0m"
+echo -e "\e[32mI am Green\e[0m"
+echo -e "\e[33mI am yellow\e[0m"
+echo -e "\e[34mI am Blue\e[0m"
+echo -e "\e[35mI am Maganta\e[0m"
+
+# forefround clour
+echo -e "\e[45;33mI am yellow\e[0m"
