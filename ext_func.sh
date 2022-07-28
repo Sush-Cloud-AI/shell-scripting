a4ea242385663e6bcabd84548d4cf73d936c52db
Sush <sushcloud12@gmail.com>
2022-07-28 02:51:13 +0100

    Shell

--- ext_func.sh
+++ ext_func.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-file_name=$0
+
 
 
 ext_fun(){
     echo "This is an function called outside this file"
-    echo "The name of the external file is $f1"
+    echo "The name of the external file is ext_func.sh"
 }
