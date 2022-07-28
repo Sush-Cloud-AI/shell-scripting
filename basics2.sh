628d8d977fd5447bfa8e40b9e314907b0b05e6c5
Sush <sushcloud12@gmail.com>
2022-07-28 21:07:23 +0100

    Shell

--- basics2.sh
+++ basics2.sh
@@ -34,4 +34,24 @@ a=10
 echo $a
 echo "${a}"
 echo ${a}
-echo '$a'
+echo '$a'    
+
+########## conditions
+
+## case
+
+read input_srting
+
+case $input_srting in
+    hello)
+        echo "hello sam"
+        ;;
+    bye)
+        echo "bye see you again"
+        ;;
+    *)
+        echo "I dont understand type hello or bye"
+esac
+
+
+
