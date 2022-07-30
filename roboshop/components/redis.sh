965a21cdf308e6bc571de4be88896245e79983cc
Sush <sushcloud12@gmail.com>
2022-07-30 22:43:44 +0100

    Shell

---
+++ roboshop/components/redis.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+source components/common.sh
+set -e  # exits the code if a cammnad fails
+COMPONENT=redis
+USER="roboshop"
+LOGFILE="/tmp/$COMPONENT.log"
+
+echo -n "Configuring the $COMPONENT repo: "
+curl -L https://raw.githubusercontent.com/stans-robot-project/$COMPONENT/main/$COMPONENT.repo -o /etc/yum.repos.d/$COMPONENT.repo
+stat $?
+
+echo -n "installing $COMPONENT: "
+yum install redis-6.2.7 -y
+stat $?
+
+echo -n "Updating the $COMPONENT listening address "
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf /etc/redis/redis.conf
+stat $?
+
+## calling start service 
+STARTING_SERV
+
+echo -e "\e[32m _________________$COMPONENT configuration is completed________________\e[0m"
