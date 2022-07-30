77d54d96aeac452fdd05bb402b5327fe1b783d55
Sush <sushcloud12@gmail.com>
2022-07-30 02:22:50 +0100

    Shell

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -4,25 +4,44 @@ set -e  # exits the code if a cammnad fails
 COMPONENT=frontend
 LOGFILE="/tmp/COMPONENT.log"
 
+## sourcing the if loop to check if the user is root or not
 source components/common.sh
 
-echo -n "Installing nginx"
-yum install nginx -y &>> $LOGFILE
-
-if [ $? -eq 0 ] ; then
+## fuction to check if the execution of command is sucessfull
+stat(){
+    if [ $1 -eq 0 ] ; then
     echo -e "\e[32m SUCCESS \e[0m"
 else
     echo -e "\e[31m FAILURE \e[0m"
 fi
+}
 
+echo -n "Installing nginx: "
+yum install nginx -y &>> $LOGFILE
+stat $?
 
-# systemctl enable nginx
-# systemctl start nginx
-# curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
-# cd /usr/share/nginx/html
-# rm -rf *
-# unzip /tmp/frontend.zip
-# mv frontend-main/* .
-# mv static/* .
-# rm -rf frontend-main README.md
-# mv localhost.conf /etc/nginx/default.d/roboshop.conf
+systemctl enable nginx &>> $LOGFILE
+
+echo -n "Starting ngnix"
+systemctl start nginx
+stat $?
+
+echo -n "Downloading $COMPONENT repo: "
+curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip" &>> $LOGFILE
+stat $?
+
+echo -n "Clearing the old content: "
+cd /usr/share/nginx/html
+rm -rf *
+stat $?
+
+echo -n "extracting the downloaded repo: "
+unzip /tmp/frontend.zip &>> $LOGFILE
+stat $?
+
+echo -n "Updating the PROXY file: "
+mv frontend-main/* .
+mv static/* .
+rm -rf frontend-main README.md
+mv localhost.conf /etc/nginx/default.d/roboshop.conf
+stat $?
