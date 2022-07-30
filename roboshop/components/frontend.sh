9d1c9a7751c4894bf1ecf0d57c4a6c4e1305af9a
Sush <sushcloud12@gmail.com>
2022-07-30 01:56:23 +0100

    Shell

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
 
 set -e  # exits the code if a cammnad fails
+COMPONENT=frontend
+LOGFILE="/tmp/COMPONENT.log"
 
 source components/common.sh
 
-yum install nginx-y
-systemctl enable nginx
-systemctl start nginx
-curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
-cd /usr/share/nginx/html
-rm -rf *
-unzip /tmp/frontend.zip
-mv frontend-main/* .
-mv static/* .
-rm -rf frontend-main README.md
-mv localhost.conf /etc/nginx/default.d/roboshop.conf
+echo -n "Installing nginx"
+yum install nginx -y &>> $LOGFILE
+
+if [ $? -eq 0 ] ; then
+    echo -e "\e[32m SUCCESS \e[0m"
+else
+    echo -e "\e[31m FAILURE \e[0m"
+fi
+
+
+# systemctl enable nginx
+# systemctl start nginx
+# curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
+# cd /usr/share/nginx/html
+# rm -rf *
+# unzip /tmp/frontend.zip
+# mv frontend-main/* .
+# mv static/* .
+# rm -rf frontend-main README.md
+# mv localhost.conf /etc/nginx/default.d/roboshop.conf
