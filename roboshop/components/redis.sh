f6a4fcaf2f5eade1d1a85eb76863e8bde917d1fe
Sush <sushcloud12@gmail.com>
2022-07-30 22:49:42 +0100

    Shell

--- roboshop/components/redis.sh
+++ roboshop/components/redis.sh
@@ -6,11 +6,11 @@ USER="roboshop"
 LOGFILE="/tmp/$COMPONENT.log"
 
 echo -n "Configuring the $COMPONENT repo: "
-curl -L https://raw.githubusercontent.com/stans-robot-project/$COMPONENT/main/$COMPONENT.repo -o /etc/yum.repos.d/$COMPONENT.repo
+curl -L https://raw.githubusercontent.com/stans-robot-project/$COMPONENT/main/$COMPONENT.repo -o /etc/yum.repos.d/$COMPONENT.repo &>> $LOGFILE
 stat $?
 
 echo -n "installing $COMPONENT: "
-yum install redis-6.2.7 -y
+yum install redis-6.2.7 -y &>> $LOGFILE
 stat $?
 
 echo -n "Updating the $COMPONENT listening address "
