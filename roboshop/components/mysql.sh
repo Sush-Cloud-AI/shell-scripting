f5e70085ecb6c6758bc2ad07b2419e068466d208
Sush <sushcloud12@gmail.com>
2022-07-31 11:14:06 +0100

    Shell

---
+++ roboshop/components/mysql.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e  # exits the code if a cammnad fails
+COMPONENT=user
+USER="mysql"
+LOGFILE="/tmp/$COMPONENT.log"
+REPO_URL="https://raw.githubusercontent.com/stans-robot-project/$COMPONENT/main/$COMPONENT.repo"
+
+## sourcing the if loop to check if the user is root or not
+source components/common.sh
+
+echo -n "Configuring $COMPONENT Repo: "
+curl -s -L -o /etc/yum.repos.d/mysql.repo $REPO_URL &>> $LOGFILE
+stat $?
+
+echo -n "Installing $COMPONENT:  "
+yum install mysql-community-server -y &>> $LOGFILE
+stat $?
+
+ech0 -n "Starting $COMPONENT: "
+systemctl enable mysqld &>> $LOGFILE && systemctl start mysqld &>> $LOGFILE
+systemctl status mysqld -l
+stat $?
