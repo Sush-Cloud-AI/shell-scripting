bcb600c97c43915a2ff0a3f6e1a83bc62bd9f020
Sush <sushcloud12@gmail.com>
2022-07-31 15:48:56 +0100

    Shell

--- roboshop/components/mysql.sh
+++ roboshop/components/mysql.sh
@@ -22,38 +22,38 @@ systemctl enable mysqld &>> $LOGFILE && systemctl start mysqld &>> $LOGFILE
 stat $?
 systemctl status mysqld -l
 
-## to validate if the password is already changed to RoboShop@1
-echo " show databases " | mysql -uroot -pRoboShop@1 &>> $LOGFILE
+# ## to validate if the password is already changed to RoboShop@1
+# echo " show databases " | mysql -uroot -pRoboShop@1 &>> $LOGFILE
 
-if [ $? -ne 0 ] ; then 
-    echo -n "Changing the default $COMPONENT root password: "
-    ## getting the tempray passwd and storing it in a variable 
-    DEFAULT_PASS=$(sudo grep "temporary password" /var/log/mysqld.log | awk '{print $NF}')  
-    ## command to set the default password and store it in a file output redirector
-    echo "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('RoboShop@1');" > /tmp/rootpsswd_chng
-    ##login and changing the default passwd 
-    mysql --connect-expired-password -uroot -p$DEFAULT_PASS  < /tmp/rootpsswd_chng &>> $LOGFILE
-    stat $?
-fi
+# if [ $? -ne 0 ] ; then 
+#     echo -n "Changing the default $COMPONENT root password: "
+#     ## getting the tempray passwd and storing it in a variable 
+#     DEFAULT_PASS=$(sudo grep "temporary password" /var/log/mysqld.log | awk '{print $NF}')  
+#     ## command to set the default password and store it in a file output redirector
+#     echo "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('RoboShop@1');" > /tmp/rootpsswd_chng
+#     ##login and changing the default passwd 
+#     mysql --connect-expired-password -uroot -p$DEFAULT_PASS  < /tmp/rootpsswd_chng &>> $LOGFILE
+#     stat $?
+# fi
 
-echo "show plugins" | mysql -uroot -pRoboShop@1 | grep validate_password &>> $LOGFILE
-if [ $? -eq 0 ] ; then 
-    echo -n "uninstall passwd validate plugin: "
-    echo "uninstall plugin validate_password;" > /tmp/pass_validate.sql &>> $LOGFILE
+# echo "show plugins" | mysql -uroot -pRoboShop@1 | grep validate_password &>> $LOGFILE
+# if [ $? -eq 0 ] ; then 
+#     echo -n "uninstall passwd validate plugin: "
+#     echo "uninstall plugin validate_password;" > /tmp/pass_validate.sql &>> $LOGFILE
     
-    mysql --connect-expired-password -uroot -pRoboShop@1 < /tmp/pass_validate.sql &>> $LOGFILE
-    stat $?
-fi
+#     mysql --connect-expired-password -uroot -pRoboShop@1 < /tmp/pass_validate.sql &>> $LOGFILE
+#     stat $?
+# fi
 
-echo -n "dwonlading the schema"
-curl -s -L -o /tmp/mysql.zip $SCHEMA_URL &>> $LOGFILE
-stat $?
+# echo -n "dwonlading the schema"
+# curl -s -L -o /tmp/mysql.zip $SCHEMA_URL &>> $LOGFILE
+# stat $?
 
-echo -n "Extracting the schema"
-cd /tmp
-unzip -o mysql.zip &>> $LOGFILE
-stat $?
+# echo -n "Extracting the schema"
+# cd /tmp
+# unzip -o mysql.zip &>> $LOGFILE
+# stat $?
 
-echo -n "injecting the schema"
-cd mysql-main && mysql -u root -pRoboShop@1 < shipping.sql &>> $LOGFILE
-stat $?
+# echo -n "injecting the schema"
+# cd mysql-main && mysql -u root -pRoboShop@1 < shipping.sql &>> $LOGFILE
+# stat $?
