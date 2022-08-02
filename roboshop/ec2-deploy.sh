3480e686c793c124b3f5c01e4dd693da0a70fd21
Sush <sushcloud12@gmail.com>
2022-08-02 21:48:00 +0100

    Shell

--- roboshop/ec2-deploy.sh
+++ roboshop/ec2-deploy.sh
@@ -5,4 +5,10 @@ SG_ID="sg-04b01b7734f241f98"
 
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=devops-conf" | jq '.Images[].ImageId' | sed -e 's/"//g')
 
-aws ec2 run-instances --security-group-ids $SG_ID  --image-id $AMI_ID --instance-type t2.micro --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$COMPONENT}]" | jq
+
+## aws ec2 run-instances --security-group-ids $SG_ID  --image-id $AMI_ID --instance-type t2.micro --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$COMPONENT}]" | jq
+
+PRIVATE_IP=$(aws ec2 run-instances --security-group-ids $SG_ID  --image-id $AMI_ID --instance-type t2.micro --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$COMPONENT}]" | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g' )
+
+echo $PRIVATE_IP
+#aws route53 change-resource-record-sets --hosted-zone-id Z000835337LZTTXF3GS8F --change-batch file://sample.json
