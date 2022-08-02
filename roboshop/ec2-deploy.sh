fcc8a7858df09d1d5e69ec70fd86e7675700e8ac
Sush <sushcloud12@gmail.com>
2022-08-02 22:03:36 +0100

    Shell

--- roboshop/ec2-deploy.sh
+++ roboshop/ec2-deploy.sh
@@ -8,11 +8,11 @@ AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=devops-conf" | jq '
 
 ## aws ec2 run-instances --security-group-ids $SG_ID  --image-id $AMI_ID --instance-type t2.micro --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$COMPONENT}]" | jq
 
-PRIVATE_IP=$(aws ec2 run-instances --security-group-ids $SG_ID  --image-id $AMI_ID --instance-type t2.micro --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$COMPONENT}]" | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g' )
+PRIV_IP=$(aws ec2 run-instances --security-group-ids $SG_ID  --image-id $AMI_ID --instance-type t2.micro --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$COMPONENT}]" | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g' )
 
 #echo $PRIVATE_IP
 
-sed -e "s/COMPONENT/$COMPONENT/" "s/PRIVATE_IP/$PRIVATE_IP/" route53.json > /tmp/record.json
+sed -e "s/COMPONENT/$COMPONENT/" "s/PRIV_IP/$PRIV_IP/" route53.json > /tmp/record.json
 
 cat /tmp/record.json
 # aws route53 change-resource-record-sets --hosted-zone-id Z000835337LZTTXF3GS8F --change-batch file:///tmp/record.json | jq
