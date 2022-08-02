7e46b58aca3bcf9645c2b9171e428ef0473bcf6d
Sush <sushcloud12@gmail.com>
2022-08-02 21:09:20 +0100

    Shell

--- roboshop/ec2-deploy.sh
+++ roboshop/ec2-deploy.sh
@@ -5,4 +5,4 @@ SG_ID="sg-04b01b7734f241f98"
 
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=devops-conf" | jq '.Images[].ImageId' | sed -e 's/"//g')
 
-aws ec2 run-instances --security--group-ids $SG_ID  --image-id $AMI_ID --instance-type t2.micro --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$COMPONENT}]" | jq
+aws ec2 run-instances --security --group-ids $SG_ID  --image-id $AMI_ID --instance-type t2.micro --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$COMPONENT}]" | jq
