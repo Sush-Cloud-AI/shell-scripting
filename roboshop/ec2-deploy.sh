1d1c544c68790c3d0e8055bb49a57ec0aebef5a0
Sush <sushcloud12@gmail.com>
2022-08-02 20:56:40 +0100

    Shell

--- roboshop/ec2-deploy.sh
+++ roboshop/ec2-deploy.sh
@@ -4,4 +4,4 @@ COMPONENT=$1
 
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=devops-conf" | jq '.Images[].ImageId' | sed -e 's/"//g')
 
-aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}]'
+aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=$COMPONENT}]'
